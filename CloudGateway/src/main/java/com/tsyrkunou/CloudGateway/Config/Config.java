1e2278b5dca660f9542c1693af7f196b46b6619b
Anton_Tsyrkunou <anton.azzot@yandex.ru>
2022-04-28 10:17:58 +0300

    03-gateway

---
+++ CloudGateway/src/main/java/com/tsyrkunou/CloudGateway/Config/Config.java
@@ -0,0 +1,14 @@
+//package com.tsyrkunou.CloudGateway.Config;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class Config {
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes().build();
+//    }
+//}
